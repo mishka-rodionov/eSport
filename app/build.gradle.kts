9d96900d9c801a5b7b69a8509c838dba87633bdc
Rodionov <mishka_rodionov@mail.ru>
2025-01-06 19:38:18 +0400

    added versions for sdk to toml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.rodionov.sportsenthusiast"
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.rodionov.sportsenthusiast"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
