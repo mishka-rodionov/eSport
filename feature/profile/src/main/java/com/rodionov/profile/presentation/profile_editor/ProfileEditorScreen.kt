60deb0e901b089dbf3c2ede3c468352299b14985
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2025-02-12 08:24:09 +0400

    added ds elements

--- feature/profile/src/main/java/com/rodionov/profile/presentation/profile_editor/ProfileEditorScreen.kt
+++ feature/profile/src/main/java/com/rodionov/profile/presentation/profile_editor/ProfileEditorScreen.kt
@@ -1,9 +1,22 @@
 package com.rodionov.profile.presentation.profile_editor
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.components.DSTextInput
 
 @Composable
 fun ProfileEditorScreen() {
-    Text(text = "Profile Editor test!")
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        DSTextInput(modifier = Modifier.fillMaxWidth())
+
+    }
 }
