5794bf828da498459d780958b42a37a586d6ce69
Rodionov <mishka_rodionov@mail.ru>
2025-05-08 13:42:59 +0400

    update project dependencies

--- app/src/main/java/com/rodionov/sportsenthusiast/MainActivity.kt
+++ app/src/main/java/com/rodionov/sportsenthusiast/MainActivity.kt
@@ -2,7 +2,9 @@ package com.rodionov.sportsenthusiast
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -31,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(1))
         setContent {
             SportsEnthusiastTheme {
                 // A surface container using the 'background' color from the theme
