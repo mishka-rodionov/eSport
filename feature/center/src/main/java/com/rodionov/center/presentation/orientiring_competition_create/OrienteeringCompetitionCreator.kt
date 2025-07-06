761bd1056033cc8a454888a1c6008fccce4b6429
Rodionov <mishka_rodionov@mail.ru>
2025-07-06 17:27:07 +0400

    DEV: added button for edit/delete participant group

--- feature/center/src/main/java/com/rodionov/center/presentation/orientiring_competition_create/OrienteeringCompetitionCreator.kt
+++ feature/center/src/main/java/com/rodionov/center/presentation/orientiring_competition_create/OrienteeringCompetitionCreator.kt
@@ -1,11 +1,17 @@
 package com.rodionov.center.presentation.orientiring_competition_create
 
 import android.app.DatePickerDialog
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,15 +19,22 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
@@ -30,6 +43,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.designsystem.components.DSTextInput
 import com.example.designsystem.components.TimePickerDialog
+import com.example.designsystem.components.clickRipple
+import com.example.designsystem.theme.Dimens
 import com.rodionov.center.data.OrienteeringCreatorEffects
 import com.rodionov.center.data.OrienteeringCreatorState
 import com.rodionov.domain.models.ParticipantGroup
@@ -146,9 +161,10 @@ private fun ParticipantGroupContent(
 
 @Composable
 fun GroupContent(participantGroup: ParticipantGroup) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
+            .height(IntrinsicSize.Min)
             .border(
                 width = 1.dp,
                 color = MaterialTheme.colorScheme.outline,
@@ -156,11 +172,34 @@ fun GroupContent(participantGroup: ParticipantGroup) {
             )
             .padding(8.dp)
     ) {
-        Text(text = "Название группы: ${participantGroup.title}")
-        Text(text = "Дистанция: ${participantGroup.distance} км.")
-        Text(text = "Кол-во КП: ${participantGroup.countOfControls}")
-//        Text(text = "Порядок КП: \n ${participantGroup.sequenceOfControl.joinToString(",")}")
-        Text(text = "Контрольное время: ${participantGroup.maxTimeInMinute} мин.")
+        Column(
+//            modifier = Modifier.weight(1F)
+        ) {
+            Text(text = "Название группы: ${participantGroup.title}")
+            Text(text = "Дистанция: ${participantGroup.distance} км.")
+            Text(text = "Кол-во КП: ${participantGroup.countOfControls}")
+            Text(text = "Контрольное время: ${participantGroup.maxTimeInMinute} мин.")
+        }
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "group edit",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "group delete",
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        }
     }
 }
 
