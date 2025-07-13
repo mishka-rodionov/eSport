afe67e3e88b7e455ccd68f6daee2d096c13bc6f1
Rodionov <mishka_rodionov@mail.ru>
2025-07-13 13:48:29 +0400

    DEV: update dependencies and fix string displays

--- core/resources/src/main/java/com/rodionov/resources/ResourceProviderImpl.kt
+++ core/resources/src/main/java/com/rodionov/resources/ResourceProviderImpl.kt
@@ -6,5 +6,5 @@ class ResourceProviderImpl(private val context: Context) : ResourceProvider {
 
     override fun getString(id: Int) = context.getString(id)
 
-    override fun getString(id: Int, vararg args: Any) = context.getString(id, args)
+    override fun getString(id: Int, vararg args: Any) = context.getString(id, *args)
 }
