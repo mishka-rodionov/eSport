68c6c564cc0429165f7e9158eb1ee25dd7736c29
Rodionov <mishka_rodionov@mail.ru>
2025-10-12 14:15:42 +0400

    DEV: added documentation

--- data/local/src/main/java/com/rodionov/local/entities/orienteering/OrienteeringCompetitionWithDetails.kt
+++ data/local/src/main/java/com/rodionov/local/entities/orienteering/OrienteeringCompetitionWithDetails.kt
@@ -1,3 +1,16 @@
+/**
+ * POJO (Plain Old Java Object) для представления соревнования по спортивному ориентированию
+ * со всеми его деталями, включая группы участников и самих участников.
+ *
+ * Этот класс используется Room для объединения данных из нескольких таблиц:
+ * - `OrienteeringCompetitionEntity` (основная информация о соревновании)
+ * - `ParticipantGroupEntity` (группы участников в соревновании)
+ * - `OrienteeringParticipantEntity` (участники в каждой группе)
+ *
+ * @property competition Основная сущность соревнования.
+ * @property groupsWithParticipants Список групп участников, каждая из которых содержит свой список участников.
+ *                                  Room автоматически выполняет вложенные запросы для заполнения этой связи.
+ */
 package com.rodionov.local.entities.orienteering
 
 import androidx.room.Embedded
--- data/local/src/main/java/com/rodionov/local/entities/orienteering/ParticipantGroupWithParticipants.kt
+++ data/local/src/main/java/com/rodionov/local/entities/orienteering/ParticipantGroupWithParticipants.kt
@@ -3,6 +3,15 @@ package com.rodionov.local.entities.orienteering
 import androidx.room.Embedded
 import androidx.room.Relation
 
+/**
+ * Промежуточный класс данных (POJO) для представления связи "один ко многим"
+ * между [ParticipantGroupEntity] и списком [OrienteeringParticipantEntity].
+ *
+ * Этот класс используется в Room для объединения группы участников с ее участниками.
+ *
+ * @property group Сущность группы участников.
+ * @property participants Список участников, принадлежащих этой группе.
+ */
 data class ParticipantGroupWithParticipants(
     @Embedded
     val group: ParticipantGroupEntity,
