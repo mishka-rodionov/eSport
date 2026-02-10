456988cc09cffabf348e280e8350b7c6f78fe70b
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2026-02-10 09:05:06 +0400

    DEV: some documentation

--- data/remote/src/main/java/com/rodionov/remote/response/user/QualificationResponse.kt
+++ data/remote/src/main/java/com/rodionov/remote/response/user/QualificationResponse.kt
@@ -4,6 +4,12 @@ import com.google.gson.annotations.SerializedName
 import com.rodionov.domain.models.KindOfSport
 import com.rodionov.domain.models.SportsCategory
 
+/**
+ * Спортивная квалификация пользователя.
+ *
+ * @property kindOfSport вид спорта, к которому относится разряд.
+ * @property sportsCategory спортивный разряд или звание.
+ */
 data class QualificationResponse(
     @SerializedName("kind_of_sport")
     val kindOfSport: KindOfSport,
--- data/remote/src/main/java/com/rodionov/remote/response/user/UserResponse.kt
+++ data/remote/src/main/java/com/rodionov/remote/response/user/UserResponse.kt
@@ -3,6 +3,24 @@ package com.rodionov.remote.response.user
 import com.google.gson.annotations.SerializedName
 import com.rodionov.domain.models.Gender
 
+/**
+ * Ответ сервера с информацией о пользователе системы.
+ *
+ * Используется при авторизации, получении профиля и других операциях,
+ * связанных с персональными данными пользователя.
+ *
+ * @property id уникальный идентификатор пользователя.
+ * @property firstName имя.
+ * @property lastName фамилия.
+ * @property middleName отчество. Может отсутствовать.
+ * @property birthDate дата рождения в строковом формате,
+ * определённом контрактом API.
+ * @property gender пол пользователя.
+ * @property photo ссылка на фотографию профиля.
+ * @property phoneNumber номер телефона. Может отсутствовать.
+ * @property email адрес электронной почты.
+ * @property qualification список спортивных квалификаций пользователя.
+ */
 data class UserResponse(
     @SerializedName("id")
     val id: String,
