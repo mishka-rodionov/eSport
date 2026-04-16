aa956496d08f5a1e863380fc26bcf4617375e9f2
mishka_rodionov <mishka_rodionov@mail.ru>
2026-04-15 17:36:09 -0700

    SE-7: fix imports

--- feature/profile/src/main/java/com/rodionov/profile/presentation/main_profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/rodionov/profile/presentation/main_profile/ProfileScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
